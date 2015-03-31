76c3bb6a83785c50be076ffda1976c4c3a4bbe61
JoshuaAcosta <Acosta.joshua03@gmail.com>
2015-03-30 23:26:36 -0400

    added tasks

--- views.py
+++ views.py
@@ -1,5 +1,5 @@
 from flask import Flask, flash, redirect, render_template, request, \
-    session, url_for
+    session, url_for, g
 
 from functools import wraps
 import sqlite3
@@ -42,3 +42,26 @@ def login():
             return redirect(url_for('tasks'))
     if request.method == 'GET':
         return render_template('login.html')
+
+
+@app.route('/tasks/')
+@login_required
+def tasks():
+    g.db = connect.db()
+    cur = g.db.execute(
+        'select name, due_date, priority, task_id from tasks where status=1'
+    )
+    open_tasks = [dict(name=row[0], due_date=row[1],
+                       priority=row[2], task_id[3]) for row in cur.fetchall()]
+    cur = g.db.execute(
+        'select name, due_date, priority, task_id from tasks where status=0'
+    )
+    closed_tasks = [dict(name=row[0], due_date=row[1],
+                         priority=row[2], task_id[3]) for row in cur.fetchall()]
+    g.db.close()
+    return render_template(
+        'tasks.html',
+        form=AddTaskForm(request.form),
+        open_tasks=open_tasks,
+        closed_tasks=closed_tasks
+    )
