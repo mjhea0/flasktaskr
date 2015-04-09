24d3929152954dd3eeccbbcd7cceebeb5099b665
Joshua Acosta <Acosta.joshua03@gmail.com>
2015-04-09 11:13:33 -0400

    added comments to _config.py

--- _config.py
+++ _config.py
@@ -1,11 +1,23 @@
+"""
+basedir returns a normalized absolutized version of the pathname of the directory
+
+DATABASE_PATH defines the full path for the database by joining basedir and DATABASE
+
+WTF_CSFR_ENABLED = TRUE prevents cross-site request forgery, makes app more secure (uses Flask-WTF extension.)
+
+Re: Secret key, use of random key generator for security. 
+"""
+
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+# configurations
 DATABASE = 'flasktaskr.db'
 USERNAME = 'admin'
 PASSWORD = 'admin'
 WTF_CSFR_ENABLED = True
-SECRET_KEY = 'my_precious'
+SECRET_KEY = 'my_precious' 
 
 DATABASE_PATH = os.path.join(basedir, DATABASE)
+
