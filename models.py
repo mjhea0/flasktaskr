aa7564c967f026b1cca5ca817a189098f913081a
JoshuaAcosta <Acosta.joshua03@gmail.com>
2015-04-12 19:38:15 -0400

    added users class on models.py

--- models.py
+++ models.py
@@ -19,3 +19,21 @@ class Task(db.Model):
 
     def __repr__(self):
         return '<name {0}>'.format(self.name)
+
+
+class User(db.Model):
+
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True, nullable=False)
+    email = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
+
+    def __init__(self, name=None, email=None, password=None):
+            self.name = name
+            self.email = email
+            self.password = password
+
+    def __repr__(self):
+        return '<User {0}>'.format(self.name)
