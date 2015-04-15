e45f13228fc3c5e4537c0e52a3e5e408055043d3
Joshua Acosta <Acosta.joshua03@gmail.com>
2015-04-15 15:23:44 -0400

    defined relationships in model and updated imports

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from views import db
 
+import datetime
 
 class Task(db.Model):
 
@@ -10,13 +11,17 @@ class Task(db.Model):
     name = db.Column(db.String, nullable=False)
     due_date = db.Column(db.Date, nullable=False)
     priority = db.Column(db.Integer, nullable=False)
+    posted_date = db.Column(db.Date,
+        default=datetime.datetime.utcnow())
     status = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
     def __init__(self, name, due_date, priority, status):
             self.name = name
             self.due_date = due_date
             self.priority = priority
             self.status = status
+            self.user_id = user_id
 
     def __repr__(self):
         return '<name {0}>'.format(self.name)
@@ -30,6 +35,7 @@ class User(db.Model):
     name = db.Column(db.String, unique=True, nullable=False)
     email = db.Column(db.String, unique=True, nullable=False)
     password = db.Column(db.String, nullable=False)
+    tasks = db.relationship('Task', backref='poster')
 
     def __init__(self, name=None, email=None, password=None):
             self.name = name
