61063b9f9fd229db11b71af243b0c27984a5ab36
Joshua Acosta <Acosta.joshua03@gmail.com>
2015-04-15 12:34:09 -0400

    added login form class and updated imports

--- forms.py
+++ forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import Form
-from wtforms import TextField, DateField, IntegerField, \
-    SelectField
-from wtforms.validators import DataRequired
+from wtforms import StringField, DateField, IntegerField, \
+    SelectField, PasswordField
+from wtforms.validators import DataRequired, Length, EqualTo
 
 
 class AddTaskForm(Form):
@@ -25,5 +25,27 @@ class RegisterForm(Form):
     name = StringField(
             'Username' ,
             validators=[DataRequired(), Length(min=6, max=25)]
+    )
+    email = StringField(
+        'Email',
+        validators=[DataRequired(), Length(min=6, max=40)]
+    )
+    password = PasswordField(
+        'Password',
+        validators = [DataRequired(), Length(min=6, max=40)])
+        confirm = PasswordField(
+            'Repeat Password',
+            [DataRequired(), EqualTo('password', message='Passwords
+                must match')]
+    )
+    
+class LoginForm(Form):
+    name = StringField(
+        'Username',
+        validators=[DataRequired()]
+    )
+    password = PasswordField(
+        'Password',
+        validators=[DataRequired()]
     )
 
