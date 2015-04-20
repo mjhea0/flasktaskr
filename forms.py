e52b72b539d2c764e4bcc34deeb7ffd1450366af
JoshuaAcosta <Acosta.joshua03@gmail.com>
2015-04-19 22:33:15 -0400

    updated forms.py

--- forms.py
+++ forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import Form
 from wtforms import StringField, DateField, IntegerField, \
     SelectField, PasswordField
-from wtforms.validators import DataRequired, Length, EqualTo
+from wtforms.validators import DataRequired, Length, EqualTo, Email
 
 
 class AddTaskForm(Form):
@@ -29,7 +29,7 @@ class RegisterForm(Form):
     )
     email = StringField(
         'Email',
-        validators=[DataRequired(), Length(min=6, max=40)]
+        validators=[DataRequired(), Email(), Length(min=6, max=40)]
     )
     password = PasswordField(
         'Password',
