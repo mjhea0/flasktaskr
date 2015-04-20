8672298da5bf8fb1945349d2b8e6f8cc74120f5f
Joshua Acosta <Acosta.joshua03@gmail.com>
2015-04-20 16:38:10 -0400

    updated register function with error handling

--- views.py
+++ views.py
@@ -4,6 +4,7 @@ from functools import wraps
 from flask import Flask, flash, redirect, render_template, request, \
     session, url_for
 from flask.ext.sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import IntegrityError
 import datetime
 
 
@@ -149,9 +150,13 @@ def register():
                 form.email.data,
                 form.password.data,
             )
-            db.session.add(new_user)
-            db.session.commit()
-            flash('Thanks for registering. Please login.')
-            return redirect(url_for('login'))
+            try:
+                db.session.add(new_user)
+                db.session.commit()
+                flash('Thanks for registering. Please login.')
+                return redirect(url_for('login'))
+            except IntegrityError:
+                error = 'That username and/or email already exist.'
+                return render_template('register.html', form=form, error=error)
     return render_template('register.html', form=form, error=error)
 
